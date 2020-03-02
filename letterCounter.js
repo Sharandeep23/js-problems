c0eb89e2e6fc315c0b2384dbcce61c85fc2720ae
Partha <Partha234@outlook.com>
2020-03-02 14:45:34 +0600

    Some commenting

--- letterCounter.js
+++ letterCounter.js
@@ -3,19 +3,19 @@ sentence = sentence.trim();
 const countObj = {};
 
 for (let i = 0; i < sentence.length; i++) {
-    let letter = sentence[i];
+  let letter = sentence[i];
 
-    // space, dot, quotes are ignored
-    if (![" ", ".", "'", '"'].includes(letter)) {
-        // If the letter is not found in the object
-        // create and set its value 1
-        // or increment by 1
-        if (!Object.keys(countObj).includes(letter)) {
-            countObj[letter] = 1;
-        } else {
-            countObj[letter]++;
-        }
+  // spaces, dotes, quotes are ignored
+  if (![" ", ".", "'", '"'].includes(letter)) {
+    // If the letter is not found in the object
+    // create and set its value 1
+    // or increment by 1
+    if (!Object.keys(countObj).includes(letter)) {
+      countObj[letter] = 1;
+    } else {
+      countObj[letter]++;
     }
+  }
 }
 
 console.log(countObj);
